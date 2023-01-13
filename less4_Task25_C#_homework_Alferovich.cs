cb4da70619d0e515503a46a957c12f8675d5e993
AlexanderA <alexander.alferovich@gmail.com>
2023-01-13 16:53:12 +0400

    First and last commit

---
+++ less4_Task25_C#_homework_Alferovich.cs
@@ -0,0 +1,11 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.WriteLine("Пожалуйста, введите число для возведения в степень: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Теперь введите степень для этого числа: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+double result = Math.Pow(num1,num2);
+Console.WriteLine($"Результат возведения числа в натуральную степень: {result}.");
